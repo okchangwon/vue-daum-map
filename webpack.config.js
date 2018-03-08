4d0e12e074c5a3d3a42cc62fd3b4a02798aa5774
changwon.ok <changwon.ok@nhn.com>
2018-03-08 12:07:46 +0900

    폴리필 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './demo/src/entry.js',
+  entry: ['babel-polyfill', './demo/src/entry.js'],
   output: {
     path: path.resolve(__dirname, 'demo/dist'),
     filename: 'demo.bundle.js'
@@ -39,10 +39,6 @@ module.exports = {
     contentBase: path.join(__dirname, "demo"),
     publicPath: '/dist',
     compress: true,
-    port: 8086,
-    hot: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
+    port: 8086
+  }
 };
